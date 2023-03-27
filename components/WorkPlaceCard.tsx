90c331b7315f4e44e70b3be182e80739f5317eca
tonytangdev <tony.tang.developer@gmail.com>
2023-03-27 23:21:11 +0200

    clean up

--- components/WorkPlaceCard.tsx
+++ components/WorkPlaceCard.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-
 type Props = {
   day: string;
   hours: number;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 import { AppItem } from "@/components/AppItem";
 import Head from "next/head";
 import Image from "next/image";
-import Link from "next/link";
 
 export default function Home() {
   return (
