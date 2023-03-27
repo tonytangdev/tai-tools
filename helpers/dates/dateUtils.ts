1836e0a34c582858cfce2a7dc646a63d3e245785
tonytangdev <tony.tang.developer@gmail.com>
2023-03-27 23:23:28 +0200

    chore: document getFirstValidWeekdayOfPreviousMonth

--- helpers/dates/dateUtils.ts
+++ helpers/dates/dateUtils.ts
@@ -3,6 +3,30 @@ import weekday from "dayjs/plugin/weekday";
 
 dayjs.extend(weekday);
 
+/**
+ * Returns the first valid weekday of the previous month.
+ * If the first day of the previous month is a Sunday, return the Monday.
+ * If the first day of the previous month is a Saturday, return the Monday.
+ * Otherwise, return the first day of the previous month.
+ * @param date The date to get the first valid weekday of the previous month for.
+ * @returns The first valid weekday of the previous month.
+ * @example
+ * getFirstValidWeekdayOfPreviousMonth(dayjs("2023-02-01"));
+ * // Returns dayjs("2023-01-02")
+ * @example
+ * getFirstValidWeekdayOfPreviousMonth(dayjs("2022-11-14"));
+ * // Returns dayjs("2022-10-03")
+ * @example
+ * getFirstValidWeekdayOfPreviousMonth(dayjs("2023-06-30"));
+ * // Returns dayjs("2023-05-01")
+ * @example
+ * getFirstValidWeekdayOfPreviousMonth(dayjs("2023-03-01"));
+ * // Returns dayjs("2023-02-01")
+ * @example
+ * getFirstValidWeekdayOfPreviousMonth(dayjs("2020-03-01"));
+ * // Returns dayjs("2020-02-03")
+ * 
+ */
 export function getFirstValidWeekdayOfPreviousMonth(
   date: Dayjs | number | Date
 ): dayjs.Dayjs {
