03ba12d28d7006cb097ef60424c32f5497bec39c
tonytangdev <tony.tang.developer@gmail.com>
2023-03-31 22:33:37 +0200

    feat: add FontAwesome and Navbar component to TaiTime page
        Add FontAwesome link to _document.tsx for icon support
        Import and render Navbar component in tai-time/index.tsx

---
+++ components/Navbar.tsx
@@ -0,0 +1,71 @@
+// components/Navbar.tsx
+import React, { useState } from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const Navbar: React.FC = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return (
+        <nav className="bg-blue-500 p-4">
+            <div className="container mx-auto">
+                <div className="flex justify-between items-center">
+                    <button
+                        className="text-white block md:hidden"
+                        onClick={toggleMenu}
+                    >
+                        <i className="fas fa-bars"></i>
+                    </button>
+                    <ul
+                        className={`${isOpen ? "translate-y-0" : "-translate-y-full"
+                            } fixed inset-0 z-10 flex flex-col justify-center items-center space-y-4 md:space-y-0 md:space-x-4 md:flex-row w-full h-full md:static md:w-auto md:bg-transparent md:p-0 bg-blue-500 transition-transform duration-300 ease-in-out transform`}
+
+                    >
+                        <li className="text-center">
+                            <Link href="/">
+                                <div className="flex items-center justify-center">
+                                    <Image
+                                        src={"/images/tai-tools-icon.png"}
+                                        alt={"tools logo"}
+                                        width={52}
+                                        height={52}
+                                    />
+                                    <span className="text-white font-bold text-2xl cursor-pointer">
+                                        Home
+                                    </span>
+                                </div>
+                            </Link>
+                        </li>
+                        <li className="text-center">
+                            <Link href="/tai-time">
+                                <div className="flex items-center justify-center">
+                                    <Image
+                                        src={"/images/tai-time-icon.png"}
+                                        alt={"tools logo"}
+                                        width={52}
+                                        height={52}
+                                    />
+                                    <span className="text-white font-bold text-2xl cursor-pointer">
+                                        Tai Time
+                                    </span>
+                                </div>
+                            </Link>
+                        </li>
+                        <button
+                            className="text-white text-3xl absolute top-4 right-4 md:hidden"
+                            onClick={toggleMenu}
+                        >
+                            &times;
+                        </button>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    );
+};
+
+export default Navbar;
--- pages/tai-time/index.tsx
+++ pages/tai-time/index.tsx
@@ -9,6 +9,7 @@ import {
   formatStateToSend,
   formReducer,
 } from "../../helpers/taiTimesUtils";
+import Navbar from "@/components/Navbar";
 
 const LAMBDA_URL =
   "https://zqabvhac9i.execute-api.eu-west-1.amazonaws.com/default/generateTimesheet-dev";
@@ -54,6 +55,7 @@ export default function TaiTime() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/tai-time-favicon.ico" />
       </Head>
+      <Navbar />
       <main className="bg-gray-100 min-h-screen">
         <div className="container mx-auto px-4 py-8">
           <h1 className="text-3xl font-bold mb-6 text-center">Tai Time</h1>
